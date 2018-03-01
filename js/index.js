62fdda1b6ae465355959afc17b55b7bbb83467b0
Nett18 <kressckerl@gmail.com>
2018-03-01 17:21:24 +0100

    Lots o' stuff
    - Made the "Join the server!" open in a new tab instead of same page.
    - Tilføjede "<meta charset="utf-8">" til head i index.html.
    - Flyttet gamelist styling til "css/gamelist.css".
    - Tilføjede "css/about.css" som css for about.html.
    - Tilføjede "css/fonts.css", til at ændre text font.
    - Fixede få syntax fejl i styling, (der resulterede i at headeren i index.html er hvid nu).
    - Tilføjede baggrund til "about.html". (Ikke sikker på om det var meningen...).
    - Tilføjede auto rickrolling igen.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
-var randomnum = 16; //Number of videos minus 1 (becouse arrays start at 0)
-x = document.getElementById("youtubevideo");
-x.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
+//var randomnum = 16; //Max integer is number of videos minus 1 (becouse arrays start at 0).
+//x = document.getElementById("youtubevideo");
+//x.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
