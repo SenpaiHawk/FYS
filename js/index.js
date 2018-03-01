6346f36ee7ffa9f152575c2d891bb2f9c889d1b5
SenpaiHawk <magnus.hgk@gmail.com>
2018-03-01 13:16:27 +0100

    Gamelist added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-var copyText = "xxx";
-copyText.select();
-  /* Copy the text inside the text field */
-document.execCommand("Copy");
+var randomnum = 16; //Number of videos minus 1 (becouse arrays start at 0)
+x = document.getElementById("youtubevideo");
+x.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
